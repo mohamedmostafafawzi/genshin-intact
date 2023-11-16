745a730566678b1b0557c82567427563a4b1c560
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2023-11-16 19:28:45 +0200

    Add Network Models under Infrastructure

---
+++ Genshin Intact/Genshin Intact/Infrastructure/Network/Models/Config.swift
@@ -0,0 +1,39 @@
+//
+//  Config.swift
+//  Genshin Intact
+//
+//  Created by Mohamed Fawzi on 11/16/23.
+//
+
+import Foundation
+
+struct Config {
+    
+    /// Base URL for API calls
+    static var baseURL: String {
+        guard let url = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            fatalError("Couldn't load base URL from info.plist")
+        }
+        return url
+    }
+    
+    /// HTTP Header keys
+    enum HTTPHeaderFieldKey: String {
+        case authorization = "Authorization"
+        case contentType = "Content-Type"
+        case acceptType = "Accept"
+        case acceptEncoding = "Accept-Encoding"
+        case acceptLanguage = "Accept-Language"
+        case country = "local-country"
+        case deviceUniqueID = "device-unique-id"
+        case appType = "app-type"
+        case appVersion = "app-version"
+    }
+    
+    /// HTTP Header values
+    enum HTTPHeaderFieldValue: String {
+        case json = "application/json"
+        case ios = "ios"
+    }
+
+}
---
+++ Genshin Intact/Genshin Intact/Infrastructure/Network/Models/HTTPMethod.swift
@@ -0,0 +1,20 @@
+//
+//  HTTPMethod.swift
+//  Genshin Intact
+//
+//  Created by Mohamed Fawzi on 11/16/23.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+    case head = "HEAD"
+    case options = "OPTIONS"
+    case trace = "TRACE"
+    case connect = "CONNECT"
+    case patch = "PATCH"
+}
